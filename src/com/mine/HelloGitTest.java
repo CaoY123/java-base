c4981a66393708a85d8d54aacef85c4bbd803118
CaoY <caoy666666@gmail.com>
2022-11-21 02:05:59 +0800

    add a class called HelloGitTest
    添加了一个类HelloGitTest

---
+++ src/com/mine/HelloGitTest.java
@@ -0,0 +1,12 @@
+package com.mine;
+
+/**
+ * @author CaoY
+ * @date 2022-11-21 1:53
+ * @description
+ */
+public class HelloGitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, 美好的git世界！我来啦！");
+    }
+}
