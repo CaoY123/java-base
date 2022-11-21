ee6121181b13f99bd3feeac1d74d21cad48310dd
CaoY <caoy666666@gmail.com>
2022-11-22 00:35:25 +0800

    增加两句感慨的话

--- src/com/mine/HelloGitTest.java
+++ src/com/mine/HelloGitTest.java
@@ -9,5 +9,7 @@ public class HelloGitTest {
     public static void main(String[] args) {
         System.out.println("Hello, 美好的git世界！我来啦！");
         System.out.println("Hello, Git世界！");
+        System.out.println("真是一个令人满意的过程！");
+        System.out.println("虽然没有那么容易");
     }
 }
