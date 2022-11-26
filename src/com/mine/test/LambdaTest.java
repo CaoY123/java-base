ca2fa89d47a4da5c844cfba3471fff217d13cc4f
CaoY <3145213279@qq.com>
2022-11-26 15:11:15 +0800

    Lambda表达式、内部类、代理类学习测试

--- src/com/mine/test/LambdaTest.java
+++ src/com/mine/test/LambdaTest.java
@@ -1,7 +1,10 @@
-package com.mine;
+package com.mine.test;
 
+import com.mine.domain.Student;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -32,8 +35,16 @@ public class LambdaTest {
         System.out.println(Arrays.toString(planets));
         System.out.println("***************************");
 
-        // 另一个例子
-        ActionListener listener = event -> System.out.println("Time is + " + Instant.ofEpochMilli(event.getWhen()));
+        // 另一个例子 - timer表达式的lambda写法
+        ActionListener listener = event -> {
+            System.out.println("the Time is + " + Instant.ofEpochMilli(event.getWhen()));
+            // 让时钟响起来
+            Toolkit.getDefaultToolkit().beep();
+        };
+        Timer timer = new Timer(1000, listener);
+        timer.start();
+        JOptionPane.showMessageDialog(null, "停止运行这个程序吗？");
+        timer.stop();
     }
 
     @Test
@@ -54,5 +65,27 @@ public class LambdaTest {
         // Predicate（谓词）函数式接口，按住ctrl + 箭头可以跳到相应的接口
         list.removeIf(e -> e == null);
         System.out.println(list);
+    }
+
+    private static Student[] students;
+
+    static {
+        Student student1 = new Student(1, "张三", 20, "男", "上海", "zs@126.com");
+        Student student2 = new Student(2, "李四", 21, "女", "北京", "ls@qq.com");
+        Student student3 = new Student(3, "王五", 19, "女", "天津", "ww@163.com");
+        Student student4 = new Student(4, "赵六", 16, "男", "深圳", "zl@126.com");
+        Student student5 = new Student(5, "候七", 17, "男", "广东", "hq@qq.com");
+        students = new Student[]{student1, student2, student3, student4, student5};
+    }
+
+    @Test
+    public void test2() {
+
+        System.out.println("排序前：");
+        System.out.println(Arrays.toString(students));
+        Arrays.sort(students, Comparator.comparing(Student::getAge));
+        System.out.println("********************************");
+        System.out.println("排序后：");
+        System.out.println(Arrays.toString(students));
     }
 }
