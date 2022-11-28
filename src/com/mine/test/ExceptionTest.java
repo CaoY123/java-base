fe7419c07ef195ece980f80429d3b2ebd4217c59
CaoY <3145213279@qq.com>
2022-11-29 01:47:55 +0800

    断言和日志简单测试

---
+++ src/com/mine/test/ExceptionTest.java
@@ -0,0 +1,12 @@
+package com.mine.test;
+
+/**
+ * @author CaoY
+ * @date 2022-11-27 16:27
+ * @description 关于异常的测试 —— 暂时没写
+ */
+public class ExceptionTest {
+    public static void main(String[] args) {
+
+    }
+}
