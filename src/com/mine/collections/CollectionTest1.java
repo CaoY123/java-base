64b91fe5b6e5f75b5d8948c56c156f8a5342ed38
caoyang <caoyang@xhqb.com>
2023-03-13 13:31:42 +0800

    集合学习1

---
+++ src/com/mine/collections/CollectionTest1.java
@@ -0,0 +1,36 @@
+package com.mine.collections;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author CaoYang
+ * @create 2023-03-13-12:43 PM
+ * @description 集合学习
+ */
+public class CollectionTest1 {
+
+    public static void main(String[] args) {
+        Collection collection = null;
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        System.out.println("删除前：");
+        System.out.println(list);
+
+        // 下面不可行
+//        System.out.println("删除后：");
+//        Iterator<Integer> it = list.iterator();
+//        it.next();
+//        it.next();
+//        it.remove();
+//        System.out.println(list);
+
+        list = new LinkedList<>();
+        if (list instanceof RandomAccess) {
+            System.out.println("支持高效的随机访问");
+        } else {
+            System.out.println("不支持高效的随机访问");
+        }
+
+    }
+}
