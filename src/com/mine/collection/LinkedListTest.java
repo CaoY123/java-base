3986e18eb95e36811e4c87c03ef9d80970ae28ca
CaoY123456 <caoy_6666@126.com>
2023-03-21 01:26:15 +0800

    LinkedList源码阅读学习一

---
+++ src/com/mine/collection/LinkedListTest.java
@@ -0,0 +1,21 @@
+package com.mine.collection;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author CaoY
+ * @date 2023-03-21 0:56
+ * @description
+ */
+public class LinkedListTest {
+
+    @Test
+    public void test1() {
+        List<Integer> list = new LinkedList<>();
+        
+    }
+
+}
